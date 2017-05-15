8c2fc8f7623a8a6edfaa0dddc775235174626a5c
Mahmudul Islam <mahmud612@gmail.com>
2017-05-15 16:44:15 +1000

    initial commit

---
+++ AtlUmbracoBundleGenerator/Extensions/AtlUmbracoBundle.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.WebPages;
+
+namespace AtlUmbracoBundleGenerator.Extensions
+{
+    public static class AtlUmbracoBundle
+    {
+        /// <summary>
+        /// Renders the styles.
+        /// </summary>
+        /// <param name="helper">The helper.</param>
+        /// <param name="virtualPath">The virtual path.</param>
+        /// <param name="additionalPaths">The additional paths.</param>
+        /// <returns></returns>
+        public static IHtmlString RenderStyles(this HtmlHelper helper, string virtualPath, params string[] additionalPaths)
+        {
+            if (BundleTable.Bundles.GetBundleFor(virtualPath) == null)
+            {
+                var bundles = new StyleBundle(virtualPath);
+                foreach (var additionalPath in additionalPaths)
+                {
+                    var path = HostingEnvironment.MapPath(additionalPath);
+                    if (path == null)
+                        throw new Exception($"File not found at path: {additionalPath}");
+
+                    var attr = File.GetAttributes(path);
+                    if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
+                    {
+                        bundles.IncludeDirectory(additionalPath, "*.css", true);
+                    }
+                    else
+                    {
+                        bundles.Include(additionalPath);
+                    }
+                }
+                BundleTable.Bundles.Add(bundles);
+            }
+            return MvcHtmlString.Create($"<link href=\"{ HttpUtility.HtmlAttributeEncode(BundleTable.Bundles.ResolveBundleUrl(virtualPath))}\" rel=\"stylesheet\" />");
+        }
+
+        /// <summary>
+        /// Renders the scripts.
+        /// </summary>
+        /// <param name="helper">The helper.</param>
+        /// <param name="virtualPath">The virtual path.</param>
+        /// <param name="additionalPaths">The additional paths.</param>
+        /// <returns></returns>
+        public static IHtmlString RenderScripts(this HtmlHelper helper, string virtualPath, params string[] additionalPaths)
+        {
+            if (BundleTable.Bundles.GetBundleFor(virtualPath) == null)
+            {
+                var bundles = new ScriptBundle(virtualPath);
+                foreach (var additionalPath in additionalPaths)
+                {
+                    var path = HostingEnvironment.MapPath(additionalPath);
+                    if (path == null)
+                        throw new Exception($"File not found at path: {additionalPath}");
+
+                    var attr = File.GetAttributes(path);
+                    if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
+                    {
+                        bundles.IncludeDirectory(additionalPath, "*.js", true);
+                    }
+                    else
+                    {
+                        bundles.Include(additionalPath);
+                    }
+                }
+                BundleTable.Bundles.Add(bundles);
+            }
+            return MvcHtmlString.Create($"<script src=\"{HttpUtility.HtmlAttributeEncode(BundleTable.Bundles.ResolveBundleUrl(virtualPath))}\"></script>");
+        }
+    }
+}
---
+++ AtlUmbracoBundleGenerator/Handlers/ApplicationEventHandler.cs
@@ -0,0 +1,59 @@
+﻿
+using System.Linq;
+using System.Web.Configuration;
+using System.Web.Hosting;
+using System.Web.Optimization;
+using Umbraco.Core;
+
+namespace AtlUmbracoBundleGenerator.Handlers
+{
+    public class ApplicationEventHandler : IApplicationEventHandler
+    {
+        public void OnApplicationInitialized(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            //override appsettings
+            AddRouteToReseredRoutes();
+
+            //register bundles
+            RegisterBundles(BundleTable.Bundles);
+        }
+
+        public void OnApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            
+        }
+
+        public void OnApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            
+        }
+
+        #region Inner Methods
+
+        /// <summary>
+        /// Adds the route (~/bundles) to resered routes.
+        /// </summary>
+        private void AddRouteToReseredRoutes()
+        {
+            var appSettings = WebConfigurationManager.AppSettings["umbracoReservedPaths"] ?? "";
+
+            if (appSettings.Split(',').Any(x => string.Equals("~/bundles", x.Trim())))
+            {
+                return;
+            }
+
+            appSettings += $"{(string.IsNullOrEmpty(appSettings) ? "" : ",")}~/bundles/";
+            WebConfigurationManager.AppSettings["umbracoReservedPaths"] = appSettings;
+        }
+
+        /// <summary>
+        /// Registers the bundles.
+        /// </summary>
+        /// <param name="bundles">The bundles.</param>
+        private void RegisterBundles(BundleCollection bundles)
+        {
+            BundleTable.EnableOptimizations = true;
+        }
+        #endregion
+    }
+}
